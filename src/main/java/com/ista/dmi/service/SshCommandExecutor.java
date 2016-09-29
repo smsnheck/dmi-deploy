251476e908530e580a47ab2c20b9d961e13987d1
Sebastian Heckmann <sebastian.heckmann.extern@ista.com>
2016-09-29 08:50:33 +0200

    extract public key to application.properties add copy all button

--- src/main/java/com/ista/dmi/service/SshCommandExecutor.java
+++ src/main/java/com/ista/dmi/service/SshCommandExecutor.java
@@ -4,29 +4,30 @@ import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.connection.channel.direct.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 @Service
 public class SshCommandExecutor {
 
   private final Logger LOGGER = LoggerFactory.getLogger(SshCommandExecutor.class);
 
+  @Value("${publicKeyFingerprint}")
+  private String publicKeyFingerprint;
+
   public int executeCommand(String command) throws IOException {
-    int result = 0;
+    int result;
     try (SSHClient sshClient = new SSHClient()) {
 
-      sshClient.addHostKeyVerifier("06:22:ef:ba:d3:6c:ea:46:61:7b:07:bc:e9:e3:8c:8e");
+      sshClient.addHostKeyVerifier(publicKeyFingerprint);
       sshClient.connect("192.168.50.10");
       sshClient.authPassword("vagrant", "vagrant".toCharArray());
       final Session session = sshClient.startSession();
       final Session.Command executionCommand = session.exec(command + " &");
 
-
       LOGGER.info(executionCommand.getInputStream().toString());
-//      executionCommand.join(5, TimeUnit.SECONDS);
       executionCommand.join();
       result = executionCommand.getExitStatus();
       sshClient.close();
--- src/main/resources/static/js/dmiDeploy.js
+++ src/main/resources/static/js/dmiDeploy.js
@@ -9,6 +9,11 @@ $('[name="buildAll"]').on('switchChange.bootstrapSwitch', function(event, state)
   $('.build').bootstrapSwitch('state', state);
 });
 
+$('[name="copyAll"]').on('switchChange.bootstrapSwitch', function(event, state) {
+  $('.copy').prop('checked', state);
+  $('.copy').bootstrapSwitch('state', state);
+});
+
 function changeBackgroundColor(color) {
   document.body.style.background = color;
 }
